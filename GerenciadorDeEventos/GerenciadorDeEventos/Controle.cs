64eaedb6189eceb75e4b7ecd92acd6742600028e
jaqueline <jaqueline.adan2002@gmail.com>
2020-06-12 13:23:33 -0300

    Classe auxílio para a transição de dados

---
+++ GerenciadorDeEventos/GerenciadorDeEventos/Controle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeEventos
+{
+    class Controle
+    {
+        //classe para auxiliar na troca de dados
+        public bool estadoAcesso;
+        public String mensagem = "";
+
+        public bool Acesso(String email, String senha)
+        {
+            //envia os dados do usuário para a verificação
+            VerificarAcesso verificarAcesso = new VerificarAcesso();
+            estadoAcesso = verificarAcesso.Verifica(email, senha);
+
+            //se houver algum erro
+            if (!verificarAcesso.Equals(""))
+            {
+                this.mensagem = verificarAcesso.mensagem;
+            }
+
+            return estadoAcesso;
+        }
+
+        public String Cadastro(String usuario, String email, String senha, String confSenha)
+        {
+            return mensagem;
+        }
+    }
+}
