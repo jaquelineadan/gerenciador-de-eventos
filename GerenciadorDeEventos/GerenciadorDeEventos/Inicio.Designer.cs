63940b5738867fab4a9087fba385e85e76efc2cd
jaqueline <jaqueline.adan2002@gmail.com>
2020-06-12 13:20:11 -0300

    Interface de acesso e cadastro

---
+++ GerenciadorDeEventos/GerenciadorDeEventos/Inicio.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace GerenciadorDeEventos
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.acesso = new System.Windows.Forms.Button();
+            this.emailAcesso = new System.Windows.Forms.TextBox();
+            this.senhaAcesso = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.confSenhaCadastro = new System.Windows.Forms.TextBox();
+            this.senhaCadastro = new System.Windows.Forms.TextBox();
+            this.cadastro = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.emailCadastro = new System.Windows.Forms.TextBox();
+            this.usuarioCadastro = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // acesso
+            // 
+            this.acesso.Location = new System.Drawing.Point(69, 234);
+            this.acesso.Name = "acesso";
+            this.acesso.Size = new System.Drawing.Size(107, 23);
+            this.acesso.TabIndex = 0;
+            this.acesso.Text = "Entrar";
+            this.acesso.UseVisualStyleBackColor = true;
+            // 
+            // emailAcesso
+            // 
+            this.emailAcesso.Location = new System.Drawing.Point(69, 155);
+            this.emailAcesso.Name = "emailAcesso";
+            this.emailAcesso.Size = new System.Drawing.Size(107, 20);
+            this.emailAcesso.TabIndex = 1;
+            // 
+            // senhaAcesso
+            // 
+            this.senhaAcesso.Location = new System.Drawing.Point(69, 193);
+            this.senhaAcesso.Name = "senhaAcesso";
+            this.senhaAcesso.Size = new System.Drawing.Size(107, 20);
+            this.senhaAcesso.TabIndex = 2;
+            this.senhaAcesso.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Senha";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(236, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Confirmação da senha";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(236, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Senha";
+            // 
+            // confSenhaCadastro
+            // 
+            this.confSenhaCadastro.Location = new System.Drawing.Point(239, 272);
+            this.confSenhaCadastro.Name = "confSenhaCadastro";
+            this.confSenhaCadastro.Size = new System.Drawing.Size(107, 20);
+            this.confSenhaCadastro.TabIndex = 7;
+            this.confSenhaCadastro.UseSystemPasswordChar = true;
+            // 
+            // senhaCadastro
+            // 
+            this.senhaCadastro.Location = new System.Drawing.Point(239, 234);
+            this.senhaCadastro.Name = "senhaCadastro";
+            this.senhaCadastro.Size = new System.Drawing.Size(107, 20);
+            this.senhaCadastro.TabIndex = 6;
+            this.senhaCadastro.UseSystemPasswordChar = true;
+            // 
+            // cadastro
+            // 
+            this.cadastro.Location = new System.Drawing.Point(239, 313);
+            this.cadastro.Name = "cadastro";
+            this.cadastro.Size = new System.Drawing.Size(107, 23);
+            this.cadastro.TabIndex = 5;
+            this.cadastro.Text = "Cadastrar";
+            this.cadastro.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(236, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(236, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Usuário";
+            // 
+            // emailCadastro
+            // 
+            this.emailCadastro.Location = new System.Drawing.Point(239, 193);
+            this.emailCadastro.Name = "emailCadastro";
+            this.emailCadastro.Size = new System.Drawing.Size(107, 20);
+            this.emailCadastro.TabIndex = 11;
+            // 
+            // usuarioCadastro
+            // 
+            this.usuarioCadastro.Location = new System.Drawing.Point(239, 155);
+            this.usuarioCadastro.Name = "usuarioCadastro";
+            this.usuarioCadastro.Size = new System.Drawing.Size(107, 20);
+            this.usuarioCadastro.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(66, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 27);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Entrar em uma conta existente";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(236, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Criar uma nova conta";
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.emailCadastro);
+            this.Controls.Add(this.usuarioCadastro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.confSenhaCadastro);
+            this.Controls.Add(this.senhaCadastro);
+            this.Controls.Add(this.cadastro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.senhaAcesso);
+            this.Controls.Add(this.emailAcesso);
+            this.Controls.Add(this.acesso);
+            this.Name = "Inicio";
+            this.Text = "Gerenciador de Eventos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button acesso;
+        private System.Windows.Forms.TextBox emailAcesso;
+        private System.Windows.Forms.TextBox senhaAcesso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox confSenhaCadastro;
+        private System.Windows.Forms.TextBox senhaCadastro;
+        private System.Windows.Forms.Button cadastro;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox emailCadastro;
+        private System.Windows.Forms.TextBox usuarioCadastro;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
