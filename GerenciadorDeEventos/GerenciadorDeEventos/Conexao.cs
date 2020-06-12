834ce2f684d507d828c540f474eb69a817a3ad61
jaqueline <jaqueline.adan2002@gmail.com>
2020-06-12 13:29:38 -0300

    Classe para conexão com banco de dados (SQL Server)

---
+++ GerenciadorDeEventos/GerenciadorDeEventos/Conexao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeEventos
+{
+    class Conexao
+    {
+        //classe para estabelecer uma conexão com o banco de dados
+        SqlConnection conexao = new SqlConnection();
+
+        public Conexao()
+        {
+            conexao.ConnectionString = @"Data Source=.\sqlexpress;Initial Catalog=BancoGE;Integrated Security=True";
+        }
+
+        public SqlConnection AbrirConexao()
+        {
+            if (conexao.State == System.Data.ConnectionState.Closed)
+            {
+                conexao.Open();
+            }
+            return conexao;
+        }
+
+        public SqlConnection FecharConexao()
+        {
+            if (conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+            return conexao;
+        }
+    }
+}
